6c7e96b1021108c3c44083992dcc33c938d9cabf
Swe_Swe_Aung <swesweaung.github2020@gmail.com>
2020-04-05 23:40:37 +0630

    teamlist

---
+++ FGM/Controllers/TeamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team_BL;
+
+namespace FGM.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult TeamList()
+        {
+            return View();
+        }
+        [HttpGet]
+        public string GetTeam()
+        {
+            TeamBL TBL = new TeamBL();
+            return TBL.GetTeam();
+        }
+    }
+}
---
+++ Team_BL/TeamBL.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using FGM_DL;
+using CommonFunction;
+
+namespace Team_BL
+{
+    public class TeamBL
+    {
+        public string GetTeam()
+        {
+            BaseDL bdl = new BaseDL();
+            Function fun = new Function();
+            SqlParameter[] prms = new SqlParameter[0];
+            DataTable DtCountry = bdl.SelectData("M_Team_Select", prms);
+            return fun.DataTableToJSONWithJSONNet(DtCountry);
+        }
+    }
+}
