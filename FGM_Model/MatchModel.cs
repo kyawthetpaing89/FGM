3aa4d38af3afddbb952869814a7c1723df1a5d4d
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-04-06 17:38:31 +0630

    ktp

---
+++ FGM_Model/MatchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGM_Model
+{
+    public class MatchModel : BaseModel
+    {
+        public string MatchDate { get; set; }
+        public string MatchTime { get; set; }
+        public string TeamID1 { get; set; }
+        public string TeamID2 { get; set; }
+    }
+}
