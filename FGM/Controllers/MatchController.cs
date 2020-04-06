568cf0f5000de0ce84a712c08885357de534ea69
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-04-06 13:53:01 +0630

    ktp

---
+++ FGM/Controllers/MatchController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace FGM.Controllers
+{
+    public class MatchController : Controller
+    {
+        // GET: Match
+        public ActionResult MatchEntry()
+        {
+            return View();
+        }
+    }
+}
