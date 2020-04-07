bc188808aa4d4df53ddf46b9d7e484a82943db0d
Swe_Swe_Aung <swesweaung.github2020@gmail.com>
2020-04-07 21:38:39 +0630

    ssa

---
+++ FGM/Controllers/UserBalanceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserBalance_BL;
+using FGM_Model;
+
+namespace FGM.Controllers
+{
+    public class UserBalanceController : Controller
+    {
+        // GET: UserBalance
+        public ActionResult UserBalanceList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string GetUserBalance()
+        {
+            UserBalanceBL ubbl = new UserBalanceBL();
+            return ubbl.GetUserBalance();
+        }
+
+        [HttpPost]
+        public string GetUserBalanceByDetailDate(string id)
+        {
+            UserBalanceBL ubbl = new UserBalanceBL();
+            UserBalanceModel UBM = new UserBalanceModel();
+            UBM.UserID = id.Split('_')[0];
+            return ubbl.GetUserBalanceByDetailDate(UBM);
+        }
+    }
+}
