71475a70ab3c5c2a85661f0fbd22a64621b2dddc
kyaw thet paing <kyawthetpaing.mm@gmail.com>
2020-04-19 20:24:25 +0630

    ကျော်ကြီး

---
+++ User_BL/UserBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using CommonFunction;
+using FGM_DL;
+using FGM_Model;
+
+namespace User_BL
+{
+    public class UserBL
+    {
+        public string GetGamblers()
+        {
+            BaseDL bdl = new BaseDL();
+            Function fun = new Function();
+            SqlParameter[] prms = new SqlParameter[0];
+            DataTable DTGamblers = bdl.SelectData("M_User_Select", prms);
+            return fun.DataTableToJSONWithJSONNet(DTGamblers);
+        }
+    }
+}
